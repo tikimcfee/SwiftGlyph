360bb5955ae74b5dfc63fe7b0fbfd15fcf5b7083
Ivan Lugo - XCode <lugo@ischemaview.com>
2023-12-19 01:15:53 -0600

    - The colorizer. - Soon the letters will have colors

--- Sources/SwiftGlyphs/NeonBindings/BasicSyntaxColorizer.swift
+++ Sources/SwiftGlyphs/NeonBindings/BasicSyntaxColorizer.swift
@@ -16,6 +16,11 @@ import SwiftTreeSitter
 
 enum ColorizerError: String, Error {
     case parseDidNotReturnTree
+    case missingKernelFunction
+    case commandQueueError
+    case createBufferError
+    case commandEncoderFailure
+    case pipelineStateFailure
 }
 
 public class BasicSyntaxColorizer: MetalLinkReader {
@@ -30,16 +35,29 @@ public class BasicSyntaxColorizer: MetalLinkReader {
     }
     
     public let link: MetalLink
+    public let commandQueue: MTLCommandQueue
     public let queryRootUrl: URL
     public let language: Language
     public let parser: Parser
     public var cachedQueries = [ColorizerQuery: Query]()
+    
+    private lazy var pipelineState = CachedValue<MTLComputePipelineState?>(update: {
+        do {
+            let function = self.link.library.makeFunction(name: "blitColorsIntoConstants")!
+            let state = try self.device.makeComputePipelineState(function: function)
+            return state
+        } catch {
+            print(error)
+            return nil
+        }
+    })
     
     public init(
         link: MetalLink,
         queryBaseURL: URL = Bundle.main.resourceURL!
     ) {
         self.link = link
+        self.commandQueue = link.device.makeCommandQueue()!
         self.queryRootUrl = queryBaseURL
             .appendingPathComponent("TreeSitterSwift_TreeSitterSwift.bundle")
             .appendingPathComponent("Contents/Resources/queries/")
@@ -48,10 +66,40 @@ public class BasicSyntaxColorizer: MetalLinkReader {
         self.parser = Parser()
     }
     
+    public func runColorizer(
+        colorizerQuery: ColorizerQuery,
+        on collection: GlyphCollection
+    ) throws {
+        // Grab the pointer as source of truth
+        let (count, pointer) = collection.instancePointerPair
+        let atlas = GlobalInstances.defaultAtlas
+        
+        // Try to rebuild the string (lol)..
+        var rebuiltString = Substring(stringLiteral: "")
+        for index in (0..<count) {
+            let instanceUnicodeData = pointer[index].unicodeHash
+            let instanceKey = atlas.builder.cacheRef.unicodeMap[instanceUnicodeData]
+            guard let instanceKey else {
+                continue
+            }
+            rebuiltString.append(contentsOf: instanceKey.glyph)
+        }
+        
+        // Make the color buffer, blitit
+        let colorBuffer = try execute(
+            colorizerQuery: colorizerQuery,
+            for: String(rebuiltString)
+        )
+        try blitColors(
+            from: colorBuffer,
+            into: collection
+        )
+    }
+    
     public func execute(
         colorizerQuery: ColorizerQuery,
         for loadedText: String
-    ) async throws -> MTLBuffer {
+    ) throws -> MTLBuffer {
         // Create the query for the known type
         let treeQuery = try cachedQueries[colorizerQuery] ?? {
             let queryURL = colorizerQuery.url(fromBase: queryRootUrl)
@@ -80,7 +128,7 @@ public class BasicSyntaxColorizer: MetalLinkReader {
             count: loadedText.count
         )
         let outputPointer = outputBuffer.boundPointer(
-            as: LFloat4.self, 
+            as: LFloat4.self,
             count: loadedText.count
         )
         outputPointer.update(
@@ -107,5 +155,43 @@ public class BasicSyntaxColorizer: MetalLinkReader {
         }
         
         return outputBuffer
+    }
+    
+    public func blitColors(
+        from colorBuffer: MTLBuffer,
+        into collection: GlyphCollection
+    ) throws {
+        guard let commandBuffer = commandQueue.makeCommandBuffer()
+        else { throw ColorizerError.createBufferError }
+        guard let commandEncoder = commandBuffer.makeComputeCommandEncoder()
+        else { throw ColorizerError.commandEncoderFailure }
+        guard let pipelineState = pipelineState.get()
+        else { throw ColorizerError.pipelineStateFailure }
+        
+        commandEncoder.label = collection.nodeId
+        commandBuffer.pushDebugGroup("[SG] Color copy \(collection.nodeId)")
+        
+        commandEncoder.setBuffer(colorBuffer,  offset: 0, index: 0)
+        commandEncoder.setBuffer(collection.instanceState.instanceBuffer, offset: 0, index: 1)
+        
+        let countBuffer = try collection.createInstanceStateCountBuffer()
+        commandEncoder.setBuffer(countBuffer, offset: 0, index: 2)
+        
+        commandEncoder.setComputePipelineState(pipelineState)
+        
+        // Setup compute groups
+        let threadgroups = {
+            let bufferElementCount = collection.instanceCount
+            let threadgroupWidth = max(pipelineState.threadExecutionWidth - 1, 1)
+            let threadgroupsNeeded = (bufferElementCount + threadgroupWidth - 1) / (threadgroupWidth)
+            return MTLSize(width: threadgroupsNeeded, height: 1, depth: 1)
+        }()
+        let threadsPerThreadgroup = MTLSize(width: pipelineState.threadExecutionWidth, height: 1, depth: 1)
+        commandEncoder.dispatchThreadgroups(threadgroups, threadsPerThreadgroup: threadsPerThreadgroup)
+
+        commandEncoder.endEncoding()
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+        commandBuffer.popDebugGroup()
     }
 }
