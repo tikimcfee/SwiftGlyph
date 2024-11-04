f152f2d4c3537276dc34be35698856cb4fbd9f1d
Ivan Lugo <ivan.lugo.dev@pm.me>
2024-11-04 08:41:48 -0600

    - macoS 15, swiftpm 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftGlyph",
     platforms: [
         .iOS(.v17),
-        .macOS(.v14),
+        .macOS(.v15),
     ],
     products: [
         .library(
